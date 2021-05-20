7aa3d1bc01df37ae02a96bbae5c525a98332dacb
cho-to <nicole2974@g.skku.edu>
2021-05-20 20:53:35 +0900

    test

--- script.js
+++ script.js
@@ -1,3 +1,85 @@
+const request = require("request"); // npm install request
+const cheerio = require("cheerio"); // npm install cheerio-httpcli 를 해야함
+
+scraped = {
+    'DATE': '',
+    'EXCHANGE RATE': '',
+    'BUY': '',
+    'SELL': ''
+}
+ var choice;
+ var array = ["USD", "EUR", "JPY", "CNY"];
+ 
+ choice = array[0]; // 여기를 고차면 index 0: USD, index 1: EUR, index 2: JPY, index 3: CNY
+
+
+function getData() {
+
+    switch (choice) {
+        case "USD":
+            request("https://finance.naver.com/marketindex/exchangeDailyQuote.nhn?marketindexCd=FX_USDKRW", function (err, res, body) {
+        const $ = cheerio.load(body);
+        const bodyList = $(".tbl_exchange tbody tr").map(function (i, element) {
+            scraped['DATE'] = parseFloat(String($(element).find('td:nth-of-type(1)').text()).replace(/,/g, "")); // 필요한 부분은 다 string에서 float로 바꿈
+            scraped['EXCHANGE RATE'] =  parseFloat(String($(element).find('td:nth-of-type(2)').text()).replace(/,/g, ""));
+            scraped['BUY'] =  parseFloat(String($(element).find('td:nth-of-type(4)').text()).replace(/,/g, ""));
+            scraped['SELL'] =  parseFloat(String($(element).find('td:nth-of-type(5)').text()).replace(/,/g, ""));
+            console.log(scraped)
+            });
+        });
+        break;
+    case "EUR":
+            request("https://finance.naver.com/marketindex/exchangeDailyQuote.nhn?marketindexCd=FX_EURKRW", function (err, res, body) {
+        const $ = cheerio.load(body);
+        const bodyList = $(".tbl_exchange tbody tr").map(function (i, element) {
+            scraped['DATE'] = parseFloat(String($(element).find('td:nth-of-type(1)').text()).replace(/,/g, ""));
+            scraped['EXCHANGE RATE'] =  parseFloat(String($(element).find('td:nth-of-type(2)').text()).replace(/,/g, ""));
+            scraped['BUY'] =  parseFloat(String($(element).find('td:nth-of-type(4)').text()).replace(/,/g, ""));
+            scraped['SELL'] =  parseFloat(String($(element).find('td:nth-of-type(5)').text()).replace(/,/g, ""));
+            console.log(scraped)
+        });
+        
+
+    });
+    break;
+    case "JPY":
+            request("https://finance.naver.com/marketindex/exchangeDailyQuote.nhn?marketindexCd=FX_JPYKRW", function (err, res, body) {
+        const $ = cheerio.load(body);
+
+        const bodyList = $(".tbl_exchange tbody tr").map(function (i, element) {
+            scraped['DATE'] = parseFloat(String($(element).find('td:nth-of-type(1)').text()).replace(/,/g, ""));
+            scraped['EXCHANGE RATE'] =  parseFloat(String($(element).find('td:nth-of-type(2)').text()).replace(/,/g, ""));
+            scraped['BUY'] =  parseFloat(String($(element).find('td:nth-of-type(4)').text()).replace(/,/g, ""));
+            scraped['SELL'] =  parseFloat(String($(element).find('td:nth-of-type(5)').text()).replace(/,/g, ""));
+            console.log(scraped)
+        });
+        
+
+    });
+    break;
+    case "CNY":
+            request("https://finance.naver.com/marketindex/exchangeDailyQuote.nhn?marketindexCd=FX_CNYKRW", function (err, res, body) {
+        const $ = cheerio.load(body);
+
+        const bodyList = $(".tbl_exchange tbody tr").map(function (i, element) {
+            scraped['DATE'] = parseFloat(String($(element).find('td:nth-of-type(1)').text()).replace(/,/g, ""));
+            scraped['EXCHANGE RATE'] =  parseFloat(String($(element).find('td:nth-of-type(2)').text()).replace(/,/g, ""));
+            scraped['BUY'] =  parseFloat(String($(element).find('td:nth-of-type(4)').text()).replace(/,/g, ""));
+            scraped['SELL'] =  parseFloat(String($(element).find('td:nth-of-type(5)').text()).replace(/,/g, ""));
+            console.log(scraped)
+        });
+        
+
+    });
+    break;
+   }
+  
+}
+
+getData();
+
+/*
+//실시간 날짜, 시간, 분을 불러오는 function
 function printDateTime() {
     let today = document.getElementById("todayDetail");
     let compare = document.getElementById("compareDetail");
@@ -15,9 +97,29 @@ function printDateTime() {
 
 }
 
+//calculate 한 result를 보여준다.
 function printCal(){
-    const input = document.getElementById("inputCal").value;
-    document.getElementById("resultCal").innerText = input;
+    let input = document.getElementById("inputCal").value;
+    let option = type.options[type.selectedIndex].innerText;
+    
+    if(option==="종류"){
+        let result = "외화 종류를 선택해주세요."; 
+    }
+    else if(option === "USD"){
+        getData("USD");
+        let result = scraped['BUY'];
+    }
+    else if(option === "JPY"){
+        getData("JPY");
+        let result = scraped['BUY'];
+    }
+    else if(option === "CNY"){
+        getData("CNY");
+        let result = scraped["CNY"];
+    }
+
+
+    document.getElementById("resultCal").innerText = result;
 }
 
 //현재 날짜를 기준으로 날짜를 선택할 수 있게 한다. 20210101~현재날짜까지 가능하도록 설정함.
@@ -90,4 +192,4 @@ type.onchange = function(){
 window.addEventListener("load", () => {
     setMonthDay();
     printDateTime();
-});
+}); */
